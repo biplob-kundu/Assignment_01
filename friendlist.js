cf0ea75ea6be999b2befb7a54043eb89652a1ffd
Biplobkundu <biploop.bd@gmail.com>
2022-11-08 11:40:57 +0600

    first commit

---
+++ friendlist.js
@@ -0,0 +1,8 @@
+let friendlist = [
+  {name: 'Selim', Age: '29', mob: '123464654'},
+  {name: 'Setu', Age: '28', mob: '3579853416'},
+  {name: 'Anupam', Age: '27', mob: '1345697864'},
+  {name: 'Sohel', Age: '28', mob: '78663456369'},
+  {name: 'Raj', Age: '29', mob: '54974234764'}
+]
+export{friendlist};
